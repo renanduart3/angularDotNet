ed7d35245f45205051011e833f66e77a2aeb5952
Renan Rosa <renan110306@gmail.com>
2020-03-26 17:45:05 -0300

    Projeto estrutura inicial

---
+++ Agil.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Agil.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Agil.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Agil.Repository
+{
+    public class Class1
+    {
+    }
+}
